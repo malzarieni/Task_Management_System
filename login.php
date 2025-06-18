25a55f8c4d8839a37e896dc440b6c1a3268fb98c
Mohammad AlZarieni <mohammadzarieni@gmail.com>
2025-06-17 23:52:35 -0500

    Log in backend
    created the databases, and the users for admin and employee control

--- login.php
+++ login.php
@@ -10,15 +10,33 @@
 </head>
 <body class="login-body">
 
-		<form class="shadow p-4">
+		<form method ="POST" action="app/login.php" class="shadow p-4">
 		  <h3 class="display-4">Log In</h3>
+		  <?php if (isset($_GET['error'])) { ?>
+		  	<div class="alert alert-danger" role="alert">
+ 			  <?php echo stripcslashes($_GET['error']); ?>
+			</div>
+		  <?php } ?>
+
+		  <?php if (isset($_GET['success'])) { ?>
+		  	<div class="alert alert-success" role="alert">
+ 			  <?php echo stripcslashes($_GET['success']); ?>
+			</div>
+		  <?php } 
+
+		  	//$pass = "123";
+		  	//$pass = password_hash($pass, PASSWORD_DEFAULT);
+		  	//echo $pass;
+
+		  ?>
+
 		  <div class="mb-3">
-		    <label for="exampleInputEmail1" class="form-label">Email address</label>
-		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+		    <label for="exampleInputEmail1" class="form-label">User name</label>
+		    <input type="text" class="form-control" name="user_name">
 		  </div>
 		  <div class="mb-3">
 		    <label for="exampleInputPassword1" class="form-label">Password</label>
-		    <input type="password" class="form-control" id="exampleInputPassword1">
+		    <input type="password" class="form-control" name="password" id="exampleInputPassword1">
 		  </div>
 		  <button type="submit" class="btn btn-primary">Log In</button>
 		</form>
